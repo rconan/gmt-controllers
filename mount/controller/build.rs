6b312d72bcbc936c12dbeb59f1df4ac19c25bfa0
Rod Conan <rconan@gmto.org>
2023-06-07 14:40:47 -0700

    MOUNT_FDR_AZ17HZ TO SELECT CONTROL SYS WITH NOTCH AT 17HZ

--- mount/controller/build.rs
+++ mount/controller/build.rs
@@ -1,4 +1,14 @@
+use std::env;
+
 fn main() {
-    let sys = simulink_rs::Sys::new(Some("MountController"));
+    let sys = if let Ok(_) = env::var("MOUNT_FDR_AZ17HZ") {
+        simulink_rs::Sys::builder()
+            .name("MountController")
+            .folder("sys-az17Hz")
+            .build()
+    } else {
+        simulink_rs::Sys::new(Some("MountController"))
+    };
     sys.compile().generate_module();
+    println!("cargo:rerun-if-env-changed=MOUNT_FDR_AZ17HZ")
 }
