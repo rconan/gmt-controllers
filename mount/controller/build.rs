6c141d09007e7a539cae9ce243bce3cb71178d8b
Rod Conan <rconan@gmto.org>
2023-06-07 17:46:12 -0700

    NEW WARNING IN MOUNT CONTROL

--- mount/controller/build.rs
+++ mount/controller/build.rs
@@ -2,12 +2,13 @@ use std::env;
 
 fn main() {
     let sys = if let Ok(_) = env::var("MOUNT_FDR_AZ17HZ") {
-        println!("cargo:warning=compiling mount control with 17Hz notch filter");
+        println!("cargo:warning=compiling ODC mount control with 17Hz notch filter");
         simulink_rs::Sys::builder()
             .name("MountController")
             .folder("sys-az17Hz")
             .build()
     } else {
+        println!("cargo:warning=compiling vanilla ODC mount control");
         simulink_rs::Sys::new(Some("MountController"))
     };
     sys.compile().generate_module();
