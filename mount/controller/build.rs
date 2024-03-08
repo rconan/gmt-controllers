cee5056a6d5506f0f1b9dc04dd4cd5807eec5b11
Rod Conan <rconan@gmto.org>
2024-03-08 14:05:32 -0800

    GMT_MOUNT-CTRL BUILD & VERSION
    Update panic message
    Move to 1.0.0 version

--- mount/controller/build.rs
+++ mount/controller/build.rs
@@ -1,6 +1,10 @@
 use std::env;
 
 fn main() {
+    if env::var("DOCS_RS").is_ok() {
+        println!("cargo:warning=DOCS_RS is set, using mount model: `MOUNT_FDR_1kHz`");
+        env::set_var("MOUNT_MODEL", "MOUNT_FDR_1kHz");
+    }
     let model_folder = match env::var("MOUNT_MODEL") {
         Ok(val) => match val.as_str() {
             "MOUNT_PDR_8kHz" => "sys_pdr_za30_8k",
@@ -9,8 +13,10 @@ fn main() {
             "MOUNT_FDR_1kHz-az17Hz" => "sys_fdr_1k-az17Hz",
             value => panic!("expected one of the following `MOUNT_MODEL`: `MOUNT_PDR_8kHz`, `MOUNT_FDR_1kHz` or `MOUNT_FDR_1kHz-az17Hz`, found {value}"),
         },
-        Err(e) => panic!("`MOUNT_MODEL` env var is not set: {e}"),
-    };
+        Err(e) => panic!(r"
+`MOUNT_MODEL` env var is not set,
+`MOUNT_MODEL` should be set to either `MOUNT_PDR_8kHz`, `MOUNT_FDR_1kHz` or `MOUNT_FDR_1kHz-az17Hz`,
+caused by {e}"),    };
 
     println!(
         "cargo:warning= Mount controller model folder: {}",
--- mount/driver/build.rs
+++ mount/driver/build.rs
@@ -1,6 +1,10 @@
 use std::env;
 
 fn main() {
+    if env::var("DOCS_RS").is_ok() {
+        println!("cargo:warning=DOCS_RS is set, using mount model: `MOUNT_FDR_1kHz`");
+        env::set_var("MOUNT_MODEL", "MOUNT_FDR_1kHz");
+    }
     let model_folder = match env::var("MOUNT_MODEL") {
         Ok(val) => match val.as_str() {
             "MOUNT_PDR_8kHz" => "sys_pdr_8k-linear",
@@ -8,7 +12,10 @@ fn main() {
             //"MOUNT_FDR_8kHz" => "sys_fdr_8k",
             value => panic!("expected one of the following `MOUNT_MODEL`: `MOUNT_PDR_8kHz`, `MOUNT_FDR_1kHz` or `MOUNT_FDR_1kHz-az17Hz`, found {value}"),
         },
-        Err(e) => panic!("`MOUNT_MODEL` env var is not set: {e}"),
+        Err(e) => panic!(r"
+`MOUNT_MODEL` env var is not set,
+`MOUNT_MODEL` should be set to either `MOUNT_PDR_8kHz`, `MOUNT_FDR_1kHz` or `MOUNT_FDR_1kHz-az17Hz`,
+caused by {e}"),
     };
 
     println!("cargo:warning= Mount drives model folder: {}", model_folder);
